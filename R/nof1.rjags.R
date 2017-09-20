25110fedd10b518fa3d7acc6bbedefdec00380cd
Michael Seo <swj8874@gmail.com>
2017-09-20 12:34:11 -0400

    revision

--- R/nof1.rjags.R
+++ R/nof1.rjags.R
@@ -1,10 +1,10 @@
 nof1.rjags <- function(nof1){
-  
+
   response <- nof1$response
   code <- if(response == "normal"){
     nof1.normal.rjags(nof1)
   } else if(response == "ordinal"){
-    nof1.ordinal.rjags(nof1) 
+    nof1.ordinal.rjags(nof1)
   } else if(response == "binomial"){
     nof1.binomial.rjags(nof1)
   } else if(response == "poisson"){
@@ -14,9 +14,9 @@ nof1.rjags <- function(nof1){
 }
 
 nof1.normal.rjags <- function(nof1){
-  
+
   with(nof1, {
-  
+
   code <- paste0("model{")
   code <- paste0(code,
                  "\n\tfor (i in 1:", nobs, ") {",
@@ -26,13 +26,13 @@ nof1.normal.rjags <- function(nof1){
   for(i in Treat.name){
     code <- paste0(code, " + beta_", i, "*Treat_", i, "[i]")
   }
-  
-  if(!is.null(knots)){
-    for(j in 1:ncol(BS)){
-      code <- paste0(code, "# + gamma", j, "* BS[i,", j, "]")
-    }
-  }
-  
+
+  # if(!is.null(knots)){
+  #   for(j in 1:ncol(BS)){
+  #     code <- paste0(code, "# + gamma", j, "* BS[i,", j, "]")
+  #   }
+  # }
+
   code <- paste0(code,
                  "\n\t\tY[i] ~ dnorm(m[i], prec)",
                  "\n\t}",
@@ -44,15 +44,15 @@ nof1.normal.rjags <- function(nof1){
                  "\n\talpha ~ ", alpha.prior[[1]], "(", alpha.prior[[2]], ",", alpha.prior[[3]], ")",
                  "\n\t#rho ~ ", rho.prior[[1]], "(", rho.prior[[2]], ",", rho.prior[[3]], ")"
                  )
-  
+
   for(i in Treat.name){
-    code <- paste0(code, "\n\tbeta_", i, " ~ ", beta.prior[[1]], "(", beta.prior[[2]], ",", beta.prior[[3]], ")")  
+    code <- paste0(code, "\n\tbeta_", i, " ~ ", beta.prior[[1]], "(", beta.prior[[2]], ",", beta.prior[[3]], ")")
   }
-  
+
   # if(!is.null(knots)){
   #   for(j in 1:ncol(BS)){
-  #     code <- paste0(code, "\n\tgamma", j, " ~ ", gamma.prior[[1]], "(", gamma.prior[[2]], ",", gamma.prior[[3]], ")")  
-  #   }    
+  #     code <- paste0(code, "\n\tgamma", j, " ~ ", gamma.prior[[1]], "(", gamma.prior[[2]], ",", gamma.prior[[3]], ")")
+  #   }
   # }
 
   code <- paste0(code, nof1.hy.prior.rjags(hy.prior), "\n}")
@@ -64,24 +64,24 @@ nof1.normal.rjags <- function(nof1){
 
 
 nof1.binomial.rjags <- function(nof1){
-  
+
   with(nof1, {
-    
+
   code <- paste0("model{")
   code <- paste0(code,
                  "\n\tfor (i in 1:", nobs, ") {",
                  "\n\t\tlogit(p[i]) <- alpha") #+ epsilon[i]")
-  
+
   for(i in Treat.name){
     code <- paste0(code, " + beta_", i, "*Treat_", i, "[i]")
   }
-  
+
   # if(!is.null(knots)){
   #   for(j in 1:ncol(BS)){
   #     code <- paste0(code, " + gamma", j, "* BS[i,", j, "]")
   #   }
   # }
-  
+
   code <- paste0(code,
                  "\n\t\tY[i] ~ dbern(p[i])",
                  "\n\t}",
@@ -93,20 +93,20 @@ nof1.binomial.rjags <- function(nof1){
                  "\n\t#}",
                  "\n\talpha ~ ", alpha.prior[[1]], "(", alpha.prior[[2]], ",", alpha.prior[[3]], ")",
                  "\n\t#rho ~ ", rho.prior[[1]], "(", rho.prior[[2]], ",", rho.prior[[3]], ")")
-  
+
   for(i in Treat.name){
-    code <- paste0(code, "\n\tbeta_", i, " ~ ", beta.prior[[1]], "(", beta.prior[[2]], ",", beta.prior[[3]], ")")  
+    code <- paste0(code, "\n\tbeta_", i, " ~ ", beta.prior[[1]], "(", beta.prior[[2]], ",", beta.prior[[3]], ")")
   }
-  
+
   # if(!is.null(knots)){
   #   for(j in 1:ncol(BS)){
-  #     code <- paste0(code, "\n\tgamma", j, " ~ ", gamma.prior[[1]], "(", gamma.prior[[2]], ",", gamma.prior[[3]], ")")  
-  #   }    
+  #     code <- paste0(code, "\n\tgamma", j, " ~ ", gamma.prior[[1]], "(", gamma.prior[[2]], ",", gamma.prior[[3]], ")")
+  #   }
   # }
-  
+
   #code <- paste0(code, nof1.hy.prior.rjags(hy.prior), "\n}")
   code <- paste0(code, "\n}")
-  
+
   return(code)
   })
 
@@ -116,7 +116,7 @@ nof1.binomial.rjags <- function(nof1){
 nof1.poisson.rjags <- function(nof1){
 
   with(nof1, {
-  
+
   code <- paste0("model{")
   code <- paste0(code,
                  "\n\tfor (i in 1:", nobs, ") {",
@@ -125,13 +125,13 @@ nof1.poisson.rjags <- function(nof1){
   for(i in Treat.name){
     code <- paste0(code, " + beta_", i, "*Treat_", i, "[i]")
   }
-  
+
   # if(!is.null(knots)){
   #   for(j in 1:ncol(BS)){
   #     code <- paste0(code, " + gamma", j, "* BS[i,", j, "]")
   #   }
   # }
-  
+
   code <- paste0(code,
                  "\n\t\tY[i] ~ dpois(lambda[i])",
                  "\n\t}",
@@ -145,25 +145,25 @@ nof1.poisson.rjags <- function(nof1){
                  "\n\t#rho ~ ", rho.prior[[1]], "(", rho.prior[[2]], ",", rho.prior[[3]], ")")
 
   for(i in Treat.name){
-    code <- paste0(code, "\n\tbeta_", i, " ~ ", beta.prior[[1]], "(", beta.prior[[2]], ",", beta.prior[[3]], ")")  
+    code <- paste0(code, "\n\tbeta_", i, " ~ ", beta.prior[[1]], "(", beta.prior[[2]], ",", beta.prior[[3]], ")")
   }
-  
+
   # if(!is.null(knots)){
   #   for(j in 1:ncol(BS)){
-  #     code <- paste0(code, "\n\tgamma", j, " ~ ", gamma.prior[[1]], "(", gamma.prior[[2]], ",", gamma.prior[[3]], ")")  
-  #   }    
+  #     code <- paste0(code, "\n\tgamma", j, " ~ ", gamma.prior[[1]], "(", gamma.prior[[2]], ",", gamma.prior[[3]], ")")
+  #   }
   # }
   #code <- paste0(code, nof1.hy.prior.rjags(hy.prior), "\n}")
   code <- paste0(code, "\n}")
   return(code)
-  }) 
+  })
 }
 
 
 nof1.ordinal.rjags <- function(nof1){
-  
+
   with(nof1, {
-    
+
     code <- paste0("model{")
     code <- paste0(code,
                    "\n\tfor (i in 1:", nobs, ") {",
@@ -175,18 +175,18 @@ nof1.ordinal.rjags <- function(nof1){
                    "\n\t\tp[i,", ncat, "] <- Q[i,", ncat-1, "]",
                    "\n\t\tfor(r in 1:", ncat-1, ") {",
                    "\n\t\t\tlogit(Q[i,r]) <- -c[r]") # + epsilon[i]")
-    
+
     for(i in Treat.name){
       code <- paste0(code, " + beta_", i, "*Treat_", i, "[i]")
     }
-    
+
     # if(!is.null(knots)){
     #   for(j in 1:ncol(BS)){
     #     code <- paste0(code, " + gamma", j, "* BS[i,", j, "]")
     #   }
     # }
-    
-    code <- paste0(code, 
+
+    code <- paste0(code,
                    "\n\t\t}",
                    "\n\t}",
                    "\n\t#e0 ~ dnorm(0, (1-rho^2)*prec)",
@@ -204,17 +204,17 @@ nof1.ordinal.rjags <- function(nof1){
                    "\n\t}",
                    "\n\tdc[1] ~ ", c1.prior[[1]], "(", c1.prior[[2]], ",", c1.prior[[3]], ")",
                    "\n\t#rho ~ ", rho.prior[[1]], "(", rho.prior[[2]], ",", rho.prior[[3]], ")")
-    
+
     for(i in Treat.name){
-      code <- paste0(code, "\n\tbeta_", i, " ~ ", beta.prior[[1]], "(", beta.prior[[2]], ",", beta.prior[[3]], ")")  
+      code <- paste0(code, "\n\tbeta_", i, " ~ ", beta.prior[[1]], "(", beta.prior[[2]], ",", beta.prior[[3]], ")")
     }
-    
+
     # if(!is.null(knots)){
     #   for(j in 1:ncol(BS)){
-    #     code <- paste0(code, "\n\tgamma", j, " ~ ", gamma.prior[[1]], "(", gamma.prior[[2]], ",", gamma.prior[[3]], ")")  
-    #   }    
+    #     code <- paste0(code, "\n\tgamma", j, " ~ ", gamma.prior[[1]], "(", gamma.prior[[2]], ",", gamma.prior[[3]], ")")
+    #   }
     # }
-    
+
     #code <- paste0(code, nof1.hy.prior.rjags(hy.prior), "\n}")
     code <- paste0(code, "\n}")
     return(code)
@@ -224,7 +224,7 @@ nof1.ordinal.rjags <- function(nof1){
 
 
 nof1.hy.prior.rjags <- function(hy.prior){
-  
+
   code <- ""
   distr <- hy.prior[[1]]
   if (distr == "dunif") {
