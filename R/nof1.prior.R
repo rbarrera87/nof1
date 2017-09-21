a16276fa02d9eed1922a2e7d3df575240d141c32
Michael Seo <swj8874@gmail.com>
2017-09-21 17:08:24 -0400

    revision

--- R/nof1.prior.R
+++ R/nof1.prior.R
@@ -1,16 +1,16 @@
 
 nof1.prior.default <- function(prior.param){
-  
+
   with(prior.param, {
-    
+
   if(is.null(alpha.prior)){
     alpha.prior <- list("dnorm", 0, 0.001)
   }
-  
+
   if(is.null(beta.prior)){
     beta.prior <- list("dnorm", 0, 0.001)
   }
-  
+
   if(is.null(gamma.prior)){
     gamma.prior <- list("dnorm", 0, 0.001)
   }
@@ -18,22 +18,24 @@ nof1.prior.default <- function(prior.param){
   if(is.null(rho.prior)){
     rho.prior <- list("dunif", -1, 1)
   }
-  
+
   if(is.null(hy.prior)){
     if(response == "binomial"){
       hy.prior <- list("dunif", 0, 2)
+    } else if (response == "normal"){
+      hy.prior <- list("dunif", 0, 20)
     } else{
-      hy.prior <- list("dgamma", 1, 0.001)  
+      hy.prior <- list("dgamma", 1, 0.001)
     }
-    
+
   }
-  
+
   prior.param.default <- list(alpha.prior = alpha.prior, beta.prior = beta.prior, gamma.prior = gamma.prior, rho.prior = rho.prior, hy.prior = hy.prior)
-  
+
   if(response == "ordinal"){
     if(is.null(dc.prior)){
       dc.prior <- list("dunif", 0, 20)
-    }     
+    }
     if(is.null(c1.prior)){
       c1.prior <- list("dunif", -20, 20)
     }
