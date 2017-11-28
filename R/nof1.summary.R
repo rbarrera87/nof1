76b1559cd0bca539ac51cdaceb014598deb57b28
Michael Seo <swj8874@gmail.com>
2017-11-28 18:18:37 -0500

    revision

--- R/nof1.summary.R
+++ R/nof1.summary.R
@@ -12,7 +12,7 @@ frequency_plot <- function(nof1){
   
   if(nof1$response %in% c("binomial", "ordinal")){
     data <- aggregate(nof1$Y, list(Y = nof1$Y, Treat = nof1$Treat), length)
-    ggplot(data= data, aes(x= Y, y= x, fill=Treat)) +  geom_bar(stat="identity", position="dodge") + ylab("Frequency") + xlim(0.5, nof1$ncat+0.5)+ theme_bw()  
+    ggplot(data= data, aes(x= Y, y= x, fill=Treat)) +  geom_bar(stat="identity", position="dodge") + ylab("Frequency") + xlim(0.5, nof1$ncat +0.5)+ theme_bw()  
   } else if(nof1$response %in% c("normal", "poisson")){
     data <- data.frame(Y = nof1$Y, Treat = nof1$Treat)
     ggplot(data, aes(x = Y, fill = Treat, color = Treat)) + geom_histogram(position = "dodge", alpha = 0.5) + theme_bw()
