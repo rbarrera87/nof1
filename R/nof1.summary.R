773097b95d12f03e30901c19f36f27ae42989bb0
Michael Seo <swj8874@gmail.com>
2017-11-29 23:36:10 -0500

    revision

--- R/nof1.summary.R
+++ R/nof1.summary.R
@@ -30,7 +30,7 @@ stacked_percent_barplot <- function(nof1){
     data <- aggregate(nof1$Y, list(Y = nof1$Y, Treat = nof1$Treat), length)
     data$ncat <- nof1$ncat
     #ggplot(data, aes(fill= Treat, y= x, x= Y)) + geom_bar( stat="identity", position="fill") + ylab("Percentage")
-    ggplot(data, aes(fill= factor(Y, 1:), y= x, x= Treat)) + geom_bar( stat="identity", position="fill") + scale_y_continuous(labels = percent_format()) + labs(fill = "Outcomes") + ylab("Percentages") +  theme_bw()  
+    ggplot(data, aes(fill= factor(Y, 1:ncat), y= x, x= Treat)) + geom_bar( stat="identity", position="fill") + scale_y_continuous(labels = percent_format()) + labs(fill = "Outcomes") + ylab("Percentages") +  theme_bw()  
   } else{
     stop("only works for binomial and ordinal data")
   }
