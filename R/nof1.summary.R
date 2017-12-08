fe9ad90a402a5e14e7edda8afea24bc63684650b
Michael Seo <swj8874@gmail.com>
2017-12-08 12:27:10 -0500

    revision

--- R/nof1.summary.R
+++ R/nof1.summary.R
@@ -168,24 +168,3 @@ probability_barplot <- function(result.list, result.name = NULL){
   
   ggplot(data, aes(fill = factor(Treat), y = probability, x = result.name)) + geom_bar( stat="identity", position="fill") + scale_y_continuous(labels = percent_format()) + labs(x = "Variables", y = "Percentages", fill = "Treatment") + coord_flip()  +  theme_bw()  
 }
-
-
-
-# 
-# plot_odds<-function(x, title = NULL){
-#   tmp<-data.frame(cbind(exp(x), exp(confint(x))))
-#   odds<-tmp[-1,]
-#   names(odds)<-c("OR", "lower", "upper")
-#   odds$vars<-row.names(odds)
-#   ticks<-c(seq(.1, 1, by =.1), seq(0, 10, by =1), seq(10, 100, by =10))
-#   
-#   ggplot(odds, aes(y= OR, x = reorder(vars, OR))) +
-#     geom_point() +
-#     geom_errorbar(aes(ymin=lower, ymax=upper), width=.2) +
-#     scale_y_log10(breaks=ticks, labels = ticks) +
-#     geom_hline(yintercept = 1, linetype=2) +
-#     coord_flip() +
-#     labs(title = title, x = "Variables", y = "OR") +
-#     theme_bw()
-# }
-
