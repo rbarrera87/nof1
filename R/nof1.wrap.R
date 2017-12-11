c482ac29d35001f998a30c4b7bde28a8bec9704a
Michael Seo <swj8874@gmail.com>
2017-12-11 11:46:46 -0500

    revision

--- R/nof1.wrap.R
+++ R/nof1.wrap.R
@@ -113,11 +113,19 @@ find_mean_difference <- function(samples, response, Treat.order){
   } 
     
   if("beta_A" %in% colnames(samples)){
-    scd <- samples[, paste0(coef_name, "_", "A")]
+    scd <- if(response == "normal"){
+      samples[,"alpha", drop = F] + samples[,"beta_A", drop = F]
+    } else if(response %in% c("binomial", "poisson")){
+      samples[, paste0(coef_name, "_A")]
+    }
   }
     
   if("beta_B" %in% colnames(samples)){
-    mscd <- samples[, paste0(coef_name, "_", "B")]
+    mscd <- if(response == "normal"){
+      samples[,"alpha", drop = F] + samples[,"beta_B", drop = F]
+    } else if(response %in% c("binomial", "poisson")){
+      samples[, paste0(coef_name, "_", "B")]
+    }
   }
     
   mean_difference <- c(base_vs_scd = mean(scd - base), base_vs_mscd = mean(mscd - base), mscd_vs_scd = mean(scd - mscd))
