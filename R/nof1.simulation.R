211b072d86381ea3f11ce246a10bdccbb85b8392
Michael Seo <swj8874@gmail.com>
2017-12-11 13:20:55 -0500

    Revert "revision"
    This reverts commit 1c991a643610cef9cf3f1dd4a37ec66ccc3b9487.

--- R/nof1.simulation.R
+++ R/nof1.simulation.R
@@ -8,7 +8,8 @@ inv_logit <- function(a){
 #'
 #' @export
 
-nof1.ordinal.simulation <- function(Base.size = 100, Treat.size = 100, alpha = 0, beta_A = -0.1, beta_B = -0.3, cut = c(0.5,1,1.5,2), ncat = 5){
+nof1.ordinal.simulation <- function(baseline = "baseline", Base.size = 100, Treat.size = 100,
+                                    alpha = 0, beta_A = -0.1, beta_B = -0.3, cut = c(0.5,1,1.5,2), ncat = 5){
   
   Treat <- rep(baseline, Base.size)
   Treat <- c(Treat, rep(c("B", "A", "B", "A"), each = Treat.size))
@@ -47,9 +48,10 @@ nof1.ordinal.simulation <- function(Base.size = 100, Treat.size = 100, alpha = 0
 #'
 #' @export
 
-nof1.binomial.simulation <- function(Base.size = 14, Treat.size = 56, alpha = 0.5, beta_A = -0.1, beta_B = -0.05){
+nof1.binomial.simulation <- function(baseline = "baseline", Base.size = 14, Treat.size = 56,
+                                     alpha = 0.5, beta_A = -0.1, beta_B = -0.05){
   
-  Treat <- rep("baseline", Base.size)
+  Treat <- rep(baseline, Base.size)
   Treat <- c(Treat, rep(c("B", "A", "B", "A"), each = Treat.size))
   nobs <- length(Treat)
   
@@ -78,9 +80,10 @@ nof1.binomial.simulation <- function(Base.size = 14, Treat.size = 56, alpha = 0.
 #'
 #' @export
 
-nof1.poisson.simulation <- function(Base.size = 14, Treat.size = 56, alpha = 1, beta_A = -0.1, beta_B = -0.05){
+nof1.poisson.simulation <- function(baseline = "baseline", Base.size = 14, Treat.size = 56,
+                                    alpha = 1, beta_A = -0.1, beta_B = -0.05){
 
-  Treat <- rep("baseline", Base.size)
+  Treat <- rep(baseline, Base.size)
   Treat <- c(Treat, rep(c("B", "A", "B", "A"), each = Treat.size))
   nobs <- length(Treat)
   
@@ -99,23 +102,3 @@ nof1.poisson.simulation <- function(Base.size = 14, Treat.size = 56, alpha = 1, 
   list(Y = Y, Treat = Treat)
 }
 
-nof1.normal.simulation <- function(Base.size = 2, Treat.size = 8, prec = 0.5, alpha = 50, beta_A = -3, beta_B = -1){
-  
-  Treat <- rep("baseline", Base.size)
-  Treat <- c(Treat, rep(c("B", "A", "B", "A"), each = Treat.size))
-  nobs <- length(Treat)
-  
-  Y <- epsilon <- mu <- rep(NA, nobs)
-    
-  for(i in 1:nobs){
-    
-    mu[i] <- alpha
-    if(Treat[i] == "A"){
-      mu[i] <- mu[i] + beta_A
-    } else if (Treat[i] == "B"){
-      mu[i] <- mu[i] + beta_B
-    }
-    Y[i] <- rnorm(1, mu[i], sqrt(1/prec))
-  }
-  list(Y = Y, Treat = Treat)
-}
