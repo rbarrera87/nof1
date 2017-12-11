5eecef05588605d64b8dd4055c0f08e29ad4fbf1
Michael Seo <swj8874@gmail.com>
2017-12-11 15:54:05 -0500

    revision

--- R/nof1.wrap.R
+++ R/nof1.wrap.R
@@ -245,7 +245,7 @@ wrap <- function(data, metadata){
   stool_frequency <- tryCatch({
     data_freq <- list(Treat = read_data$Treatment, Y = read_data$stool_frequency)
     nof1_freq <- with(data_freq, {
-      nof1.data(Y, Treat, Treat.order = c("baseline", "A", "B"), response = "poisson")
+      nof1.data(Y, Treat, response = "poisson")
     })
     result_freq <- nof1.run(nof1_freq)
     summarize_nof1(nof1_freq, result_freq)
@@ -256,7 +256,7 @@ wrap <- function(data, metadata){
   stool_consistency <- tryCatch({
     data_cons <- list(Treat = read_data$Treatment, Y = read_data$stool_consistency)
     nof1_cons <- with(data_cons, {
-      nof1.data(Y, Treat, Treat.order = c("baseline", "A", "B"), response = "binomial")
+      nof1.data(Y, Treat, response = "binomial")
     })
     result_cons <- nof1.run(nof1_cons)
     summarize_nof1(nof1_cons, result_cons)
@@ -267,7 +267,7 @@ wrap <- function(data, metadata){
   pain_interference <- tryCatch({
     data_pain <- list(Treat = read_data$Treatment_weekly, Y = read_data$pain_interference)
     nof1_pain <- with(data_pain, {
-      nof1.data(Y, Treat, Treat.order = c("baseline", "A", "B"), response = "normal")
+      nof1.data(Y, Treat, response = "normal")
     })
     result_pain <- nof1.run(nof1_pain)
     summarize_nof1(nof1_pain, result_pain)
@@ -278,7 +278,7 @@ wrap <- function(data, metadata){
   gi_symptoms <- tryCatch({
     data_gi <- list(Treat = read_data$Treatment_weekly, Y = read_data$gi_symptoms)
     nof1_gi <- with(data_gi, {
-      nof1.data(Y, Treat, Treat.order = c("baseline", "A", "B"), response = "normal")
+      nof1.data(Y, Treat, response = "normal")
     })
     result_gi <- nof1.run(nof1_gi)
     summarize_nof1(nof1_gi, result_gi)
